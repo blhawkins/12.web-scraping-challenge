5625dcf7dfa255b271cc0d1c6cecdcb6409c2102
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2020-10-21 16:50:04 -0500

    Updated content update

--- app.py
+++ app.py
@@ -11,16 +11,15 @@ mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
 @app.route('/')
 def index():
     #Find one record of data from the Mongo database
-    scraped_data = mongo.db.listings.find_one()
+    scraped_data = mongo.db.scraped_data.find()
     return render_template('index.html', scraped_data = scraped_data)
 
 #Route that initiates the scraping process
 @app.route('/scrape')
 def scrape():
     #Run the scrape function
-    scraped_data = mongo.db.scraped_data
     current_scrape = scrape_mars.scrape()
-    scraped_data.update({}, current_scrape, upsert=True)
+    mongo.db.scraped_data.update({}, current_scrape, upsert = True)
     return redirect('/', code = 302)
 
 if __name__ == '__main__':
